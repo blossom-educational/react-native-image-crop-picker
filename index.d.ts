dd02e90931aaa53c905d316a5d21438628bd00c3
Radek Czemerys <radko93@gmail.com>
2019-11-19 17:56:23 +0100

    Add TS type

--- index.d.ts
+++ index.d.ts
@@ -33,6 +33,7 @@ declare module "react-native-image-crop-picker" {
         enableRotationGesture?: boolean;
         cropperCancelText?: string;
         cropperChooseText?: string;
+        respectExifRotation?: boolean;
     }
 
     export interface Image {
